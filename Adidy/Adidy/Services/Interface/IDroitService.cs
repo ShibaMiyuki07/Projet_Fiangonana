71a4c00393209998343fc566d3718b8239737b38
Manitra <94074919+ShibaMiyuki07@users.noreply.github.com>
2024-09-04 20:39:30 +0300

    Ajout du service droit

---
+++ Adidy/Adidy/Services/Interface/IDroitService.cs
@@ -0,0 +1,11 @@
+﻿using Modele;
+
+namespace Adidy.Services.Interface
+{
+    public interface IDroitService
+    {
+        public Task<IEnumerable<Droit>> GetAllDroit();
+
+        public Task<IEnumerable<Droit>> DroitNotInUtilisateur(Utilisateur? user);
+    }
+}
